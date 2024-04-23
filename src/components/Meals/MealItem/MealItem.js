58948cbaeb3f7811c78fb57ba7c6795bd3287f3f
Nidnidh <Bhattnidhi400@gmail.com>
2024-04-23 11:22:38 +0530

    Your descriptive commit message here

---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,21 @@
+
+import classes from './MealItem.module.css';
+
+const MealItem = props => {
+   // eslint-disable-next-line no-unused-vars
+   const price = `$${props.price.toFixed(2)}`;
+
+
+    return (
+        <li className = {classes.meal}>
+        <div><h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div>{price}</div>
+
+        </div>
+        <div></div>
+    </li>
+    )
+    
+};
+export default MealItem;
